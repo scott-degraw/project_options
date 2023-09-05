634bb3d7f4568b298d7429dedc317d815b9de651
scott-degraw <108081974+scott-degraw@users.noreply.github.com>
2023-09-05 10:42:15 +0100

    Quick nvcc unknown option fix

--- src/Common.cmake
+++ src/Common.cmake
@@ -112,24 +112,24 @@ macro(common_project_options ENABLE_COMPILE_COMMANDS_SYMLINK)
   if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
     if(WIN32)
       # On Windows cuda nvcc uses cl and not clang
-      add_compile_options(
-        $<$<COMPILE_LANGUAGE:C>:-fcolor-diagnostics> $<$<COMPILE_LANGUAGE:CXX>:-fcolor-diagnostics>
-      )
+#      add_compile_options(
+#        $<$<COMPILE_LANGUAGE:C>:-fcolor-diagnostics> $<$<COMPILE_LANGUAGE:CXX>:-fcolor-diagnostics>
+#      )
     else()
-      add_compile_options(-fcolor-diagnostics)
+#      add_compile_options(-fcolor-diagnostics)
     endif()
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     if(WIN32)
       # On Windows cuda nvcc uses cl and not gcc
-      add_compile_options(
-        $<$<COMPILE_LANGUAGE:C>:-fdiagnostics-color=always>
-        $<$<COMPILE_LANGUAGE:CXX>:-fdiagnostics-color=always>
-      )
+#      add_compile_options(
+#        $<$<COMPILE_LANGUAGE:C>:-fdiagnostics-color=always>
+#        $<$<COMPILE_LANGUAGE:CXX>:-fdiagnostics-color=always>
+#      )
     else()
-      add_compile_options(-fdiagnostics-color=always)
+#      add_compile_options(-fdiagnostics-color=always)
     endif()
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND MSVC_VERSION GREATER 1900)
-    add_compile_options(/diagnostics:column)
+#    add_compile_options(/diagnostics:column)
   else()
     message(STATUS "No colored compiler diagnostic set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
   endif()
